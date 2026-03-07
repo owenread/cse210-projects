9393bb01f8c473bb84798ce9b59ffdb375659b22
owenread <oread01p@gmail.com>
2026-03-06 22:20:59 -0700

    Finished exercise 3 and added a guess counter

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,37 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+
+        int guess = -1;
+
+        int guessCount = 0;
+
+        while (guess != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            guessCount += 1;
+
+            if (magicNumber > guess)
+            {
+                Console.WriteLine("Guess Higher!");
+            }
+            else if (magicNumber < guess)
+            {
+                Console.WriteLine("Guess lower!");
+            }
+            else
+            {
+                Console.WriteLine("That's correct! Great job!");
+                Console.WriteLine($"It took you {guessCount} tries");
+            }
+        }
     }
 }
