a59fd9faa7cd0930f9450508dda76c81d2052e7f
owenread <oread01p@gmail.com>
2026-04-02 00:24:32 -0600

    Homework learning assignment finished

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment a = new Assignment("Samuel Bennett", "Multiplication");
+        Console.WriteLine(a.GetSummary());
+
+        MathAssignment m = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        Console.WriteLine(m.GetSummary());
+        Console.WriteLine(m.GetHomeworkList());
+
+        WritingAssignment w = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(w.GetSummary());
+        Console.WriteLine(w.GetWritingInformation());
     }
 }
